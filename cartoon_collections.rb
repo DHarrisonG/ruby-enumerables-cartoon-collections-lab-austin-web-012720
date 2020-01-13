489b682f6615a3809651e0ad650753ae105ba6b3
Doug Harrison <dougharrisong@gmail.com>
2020-01-13 16:55:10 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -14,5 +14,5 @@ end
 
 def find_the_cheese(arr)
   cheese_types = ["cheddar", "gouda", "camembert"]
-  arr.find {|cheese| cheese == "cheddar"}
+  return arr.find {|cheese| cheese == "cheddar"}
 end
