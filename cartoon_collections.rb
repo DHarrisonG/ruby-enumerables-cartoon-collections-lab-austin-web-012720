85f5681836965033ebc3a2be07eeb453ec16ddcf
Doug Harrison <dougharrisong@gmail.com>
2020-01-13 16:29:41 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -8,8 +8,8 @@ def summon_captain_planet(arr)
   arr.map {|i| "#{i.capitalize}!"}
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(arr)
+  if()
 end
 
 def find_the_cheese# code an argument here
